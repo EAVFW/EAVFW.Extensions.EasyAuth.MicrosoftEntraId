c8ca2a21e68549e8f83eb4003f1228f385f6f3ce
Simon S. Pedersen <ssp@delegate.dk>
2024-05-16 14:57:12 +0200

    Added functionality

--- src/EAVFW.Extensions.EasyAuth.MicrosoftEntraId/MicrosoftEntraEasyAuthProvider.cs
+++ src/EAVFW.Extensions.EasyAuth.MicrosoftEntraId/MicrosoftEntraEasyAuthProvider.cs
@@ -1,12 +1,26 @@
-﻿using EAVFramework.Authentication;
+using EAVFramework.Authentication;
+using EAVFramework.Extensions;
+using IdentityModel;
 using IdentityModel.Client;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Sockets;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using static IdentityModel.OidcConstants;
+using static System.Net.WebRequestMethods;
 
 namespace EAVFW.Extensions.EasyAuth.MicrosoftEntraId
 {
@@ -14,6 +28,7 @@ namespace EAVFW.Extensions.EasyAuth.MicrosoftEntraId
     public class MicrosoftEntraEasyAuthProvider : IEasyAuthProvider
     {
         private readonly IOptions<MicrosoftEntraIdEasyAuthOptions> _options;
+        private readonly IHttpClientFactory _clientFactory;
 
         public string AuthenticationName => "MicrosoftEntraId";
 
@@ -23,38 +38,56 @@ namespace EAVFW.Extensions.EasyAuth.MicrosoftEntraId
 
         public MicrosoftEntraEasyAuthProvider() { }
 
-        public MicrosoftEntraEasyAuthProvider(IOptions<MicrosoftEntraIdEasyAuthOptions> options)
+        public MicrosoftEntraEasyAuthProvider(IOptions<MicrosoftEntraIdEasyAuthOptions> options, IHttpClientFactory clientFactory)
         {
             _options = options ?? throw new System.ArgumentNullException(nameof(options));
+            _clientFactory = clientFactory ?? throw new ArgumentNullException(nameof(clientFactory));
         }
 
         public async Task OnAuthenticate(HttpContext httpcontext, string handleId, string redirectUrl)
         {
             var email = httpcontext.Request.Query["email"].FirstOrDefault();
             var redirectUri = httpcontext.Request.Query["redirectUri"].FirstOrDefault();
-
-            //  var url =$"{oauthEndpoint}"
+            
             var ru = new RequestUrl(_options.Value.AuthorizationUrl);
-         
-             //var authUri = ru.CreateAuthorizeUrl(_options.Value.ClientI
-             //  responseType: ResponseTypes.Code,
-             //  redirectUri: _options.Value.RedirectUri,
-             //  responseMode: ResponseModes.FormPost ,
-               
-             // // extra: new Parameters { { "consentId", provider.ExternalId } },
-             //  //  codeChallenge: challenge,
-             //  //  nonce: nonce,
-             //  //    responseMode: ResponseModes.FormPost,
-             // //scope: "payments:inbound payments:outbound accounts offline_access",
-
-             //  state: handleId);
-          
-           
+            var authUri = ru.CreateAuthorizeUrl(
+              clientId: _options.Value.ClientId,
+              redirectUri: _options.Value.RedirectUrl,
+              responseType: ResponseTypes.Code,
+              responseMode: ResponseModes.FormPost,
+              scope: _options.Value.Scope,
+              state: handleId + "&" + redirectUri);
+            httpcontext.Response.Redirect(authUri);
         }
 
-        public Task<(ClaimsPrincipal, string, string)> OnCallback(HttpContext httpcontext)
+        public async Task<(ClaimsPrincipal, string, string)> OnCallback(HttpContext httpcontext)
         {
-            throw new System.NotImplementedException();
+            var m = new IdentityModel.Client.AuthorizeResponse(await new StreamReader(httpcontext.Request.Body).ReadToEndAsync());
+            var state = m.State.Split(new char[] { '&' }, 2);
+            var handleId = state[0];
+            var redirectUri = state[1];
+            var http = _clientFactory.CreateClient();
+            var response = await http.RequestAuthorizationCodeTokenAsync(new AuthorizationCodeTokenRequest
+            {
+                Address = _options.Value.TokenEndpoint,
+                ClientId = _options.Value.ClientId,
+                ClientSecret = _options.Value.ClientSecret,
+                Code = m.Code,
+                RedirectUri = _options.Value.RedirectUrl,
+            });
+
+            var handler = new JwtSecurityTokenHandler(); 
+            var jwtSecurityToken = handler.ReadJwtToken(response.IdentityToken);
+            var jti = jwtSecurityToken.Claims.First(claim => claim.Type == "email").Value;
+
+            ClaimsPrincipal identity = await _options.Value.ValidateUserAsync(httpcontext, handleId, response);
+
+            if (identity == null)
+            {
+                httpcontext.Response.Redirect("error=access_denied&error_subcode=user_not_found");
+                //return;
+            }
+            return await Task.FromResult((new ClaimsPrincipal(identity), redirectUri, handleId));
         }
 
         public RequestDelegate OnSignedOut()
--- src/EAVFW.Extensions.EasyAuth.MicrosoftEntraId/MicrosoftEntraIdEasyAuthOptions.cs
+++ src/EAVFW.Extensions.EasyAuth.MicrosoftEntraId/MicrosoftEntraIdEasyAuthOptions.cs
@@ -1,9 +1,23 @@
-﻿namespace EAVFW.Extensions.EasyAuth.MicrosoftEntraId
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace EAVFW.Extensions.EasyAuth.MicrosoftEntraId
 {
     public class MicrosoftEntraIdEasyAuthOptions
     {
         public string AuthorizationUrl { get;  set; }
         public string ClientId { get;  set; }
-        public string RedirectUri { get;  set; }
+        public string ClientSecret { get;  set; }
+        public string TenantId { get;  set; }
+        public string GroupId { get;  set; }
+        public string Scope { get;  set; }
+        public string TokenEndpoint { get;  set; }
+        public string RedirectUrl { get;  set; }
+
+        public Func<HttpContext, string, TokenResponse, Task<ClaimsPrincipal>> ValidateUserAsync { get; set; }
     }
 }
